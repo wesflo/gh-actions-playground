b587bdaf491ebef2cedf203dde5c190018baac71
wesflo <info@wesflo.de>
2025-12-23 12:08:08 +0100

    try

--- src/component/gh-hello.ts
+++ src/component/gh-hello.ts
@@ -29,7 +29,7 @@ export class GhHello extends LitElement {
   `;
 
     @property({ type: String }) name: string = 'World';
-    @state() startCount: number = 1;
+    @state() startCount: number = 2;
 
     render() {
         return html`
